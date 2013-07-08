5f19bf6efbea87d0714a286ead4bbebbf3b24f1e
Nick Thomas <nrthomas@gmail.com>
2013-07-08 02:25:57 -0700

    Checkpoint

--- scripts/updates/create-channel-switch-mar.py
+++ scripts/updates/create-channel-switch-mar.py
@@ -4,10 +4,12 @@
 # for channel switching, requires python 2.7+, wget, mar utility
 
 import logging
-from os import path, makedirs
-from shutil import rmtree
-import sys
+from os import path, makedirs, walk, getcwd, chdir
+from shutil import rmtree, copytree
 import site
+import subprocess
+import sys
+import urllib
 
 site.addsitedir(path.join(path.dirname(__file__), "../../lib/python"))
 site.addsitedir(path.join(path.dirname(__file__), "../../lib/python/vendor"))
@@ -15,7 +17,7 @@ site.addsitedir(path.join(path.dirname(__file__), "../../lib/python/vendor"))
 import requests
 #from release.updates.snippets import createSnippet, getSnippetPaths
 
-MAR_URL = "http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/%(dir)s/firefox-%(version)s.%(locale)s.%(platform)s.complete.mar"
+MAR_URL = "http://stage.mozilla.org/pub/mozilla.org/firefox/nightly/%(dir)s/firefox-%(version)s.%(locale)s.%(platform)s.complete.mar"
 CHANNEL_INFO = {
     'nightly':
         {
@@ -30,16 +32,43 @@ CHANNEL_INFO = {
             'version_url': 'http://hg.mozilla.org/releases/mozilla-aurora/raw-file/default/browser/config/version.txt',
         }
 }
-WORKDIR = 'upload'
-WORKDIR_SNIPPETS = path.join(WORKDIR, 'snippets')
-WORKDIR_MAR = path.join(WORKDIR, 'ftp')
+WORKDIR = 'working'
+WORKDIR_NEW_FILES = path.join(WORKDIR, 'new_files')
+WORKDIR_UNPACK = path.join(WORKDIR, 'unpacked')
+WORKDIR_MAR = path.join(WORKDIR, 'upload', 'ftp')
+WORKDIR_SNIPPETS = path.join(WORKDIR, 'upload', 'snippets')
 
 def setup_newfiles(channel, mac=False):
+    makedirs(WORKDIR_NEW_FILES)
     log.info('Creating defaults/pref/channel-prefs.js')
+    makedirs(path.join(WORKDIR_NEW_FILES, 'defaults', 'pref'))
+    f = open(path.join(WORKDIR_NEW_FILES, 'defaults', 'pref', 'channel-prefs.js'), 'w')
+    f.write('pref("app.update.channel", "%s");\n' % channel)
+    f.close()
     log.info('Creating update-settings.ini')
+    f = open(path.join(WORKDIR_NEW_FILES, 'update-settings.ini'), 'w')
+    f.write(
+"""; If you modify this file updates may fail.
+; Do not modify this file.
+
+[Settings]
+ACCEPTED_MAR_CHANNEL_IDS=%s
+""" % CHANNEL_INFO[channel]['channel_id'])
+    f.close()
+
     if mac:
         log.info('Duplicating for silly old mac')
-    return ['foo', 'bar']
+        copytree(WORKDIR_NEW_FILES, path.join(WORKDIR_NEW_FILES, 'Contents', 'MacOS'))
+
+    new_files = []
+    for root, dir, files in walk(WORKDIR_NEW_FILES):
+        for f in files:
+            new_files.append( path.join(root, f)[len(WORKDIR_NEW_FILES)+1:])
+
+    return sorted(new_files, reverse=True)
+
+def bzipFile(file):
+    return file
 
 def getVersion(channel):
     log.debug('Getting version for channel %s' % channel)
@@ -53,7 +82,7 @@ def getVersion(channel):
         log.error('Channel %s not setup, add it to CHANNEL_INFO' % channel)
         sys.exit(1)
 
-def getUrl(channel, locale, platform, version):
+def constructUrl(channel, locale, platform, version):
     log.debug('Generating url for (%s,%s,%s)' % (channel, locale, platform))
     url = MAR_URL % {
         'dir': CHANNEL_INFO[channel]['dir'],
@@ -64,19 +93,32 @@ def getUrl(channel, locale, platform, version):
     log.debug('returning %s' % url)
     return url
 
-def download_mar(url):
+def download_file(url, dest=WORKDIR):
     log.info('Downloading %s' % url)
-    # call wget, use requests ?
-    return 'some-file'
+    dest_dir = path.dirname(dest)
+    if not path.exists(dest_dir):
+        makedirs(dest_dir)
+    urllib.urlretrieve(url, dest)
+    return dest
 
 def unpack_mar(file):
+    # files are still bzip compressed
     log.info('Unpacking %s' % file)
+    file = path.abspath(file)
+    cwd = getcwd()
+    chdir(WORKDIR_UNPACK)
+    p = subprocess.check_output(['mar', '-x', file], stderr=subprocess.STDOUT)
+    chdir(cwd)
+    return
 
 def modify_manifest(new_files):
     # one, or both of these ?
     log.info('Parsing %s' % 'uhhhhhh')
     log.info('Adding new files')
     return ['baz', 'boop']
+
+def getFileList():
+    pass
 
 def create_mar(mar_file, files):
     log.info('Creating %s' % mar_file)
@@ -90,14 +132,13 @@ def create_snippet(mar_file):
 
 def repack_mar(locale, platform, new_files):
     # create tmp dir
-    mar_url = getUrl(args.to_channel, locale, platform, args.to_version)
-    r = requests.head(mar_url)
-    r.raise_for_status()
+    mar_url = constructUrl(args.to_channel, locale, platform, args.to_version)
+    mar_file = download_file(mar_url, dest=path.join(WORKDIR, 'download', path.basename(mar_url)))
+    if path.exists(WORKDIR_UNPACK):
+        rmtree(WORKDIR_UNPACK)
+    copytree(WORKDIR_NEW_FILES, WORKDIR_UNPACK)
+    unpack_mar(mar_file)
     return
-    #mar_file = download_mar(mar_url)
-    #unpack_mar(mar_file)
-    #for f in new_files:
-    #    log.info('Copying in %s' % f)
     #file_list = modify_manifest(new_files)
     #new_mar_file = os.path.join(UPLOAD_DIR, os.path.basename(mar_url))
     #create_mar(new_mar_file, file_list)
@@ -139,18 +180,20 @@ if __name__ == '__main__':
         # delete left over nonce
         pass
 
+    if path.exists(WORKDIR):
+        log.debug('Removing workdir %s' % path.abspath(WORKDIR))
+        rmtree(WORKDIR)
+    #makedirs(WORKDIR_SNIPPETS)
+    #makedirs(WORKDIR_MAR)
+
     args.from_version  = getVersion(args.from_channel)
     args.to_version = getVersion(args.to_channel)
-    new_files = setup_newfiles(args.to_channel)
-
-    if path.exists(WORKDIR):
-        log.debug('Removing workdir' % path.abspath(WORKDIR))
-        rmtree(WORKDIR)
-    makedirs(WORKDIR_SNIPPETS)
-    makedirs(WORKDIR_MAR)
+    new_files = setup_newfiles(args.to_channel, mac=True)
 
     for locale in args.locale:
         for platform in args.platform:
+            log.info('Working on %s, %s' % (locale, platform))
             repack_mar(locale, platform, new_files)
-        
+
+    log.info("All done.")
     
